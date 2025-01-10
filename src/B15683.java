494fc9f34ff5fade573de0b4b7e6b8db39643281
Wonbin <wonbin3977@gmail.com>
2025-01-11 01:09:20 +0900

    B15683

--- src/B15683.java
+++ src/B15683.java
@@ -1,35 +1,39 @@
 import java.util.Scanner;
 
-public class B15683 
+public class B15683
 {
     public static int cctv_cnt=0; //cctv의 개수
     public static int minBlindSpot = Integer.MAX_VALUE; // 최소 사각지대 크기
+    public static int[][] cctvs;
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt(); //세로
         int m=sc.nextInt(); //가로
         int[][] work_place=new int[n][m]; //사무실
+        cctvs=new int[8][3]; //위치와 타입 저장
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 work_place[i][j]=sc.nextInt();
-                if(work_place[i][j]!=0&&work_place[i][j]!=6)
+                if(work_place[i][j]>=1&&work_place[i][j]<=5)
                 {
-                    cctv_cnt++;
+                    cctvs[cctv_cnt][0]=i; //cctv의 행
+                    cctvs[cctv_cnt][1]=j; //cctv의 열
+                    cctvs[cctv_cnt][2]=work_place[i][j]; //cctv의 타입
+                    cctv_cnt++; //cctv 개수 증가
                 }
             }
         }
         Findmin(work_place, 0);
         System.out.println(minBlindSpot);
-
         sc.close();
     }
-    public static int Findmin(int[][] work_place,int cnt)
+    public static void Findmin(int[][] work_place,int cnt)
     {
-        int n=work_place.length;
-        int m=work_place[0].length;
+        int n=work_place.length; //가로
+        int m=work_place[0].length; //세로
         if(cnt==cctv_cnt)
         {
             int count=0;
@@ -44,122 +48,82 @@ public class B15683
                 }
             }
             minBlindSpot=Math.min(minBlindSpot,count);
-            return count;
+            return;
         }
-        int[][] newboard=copyboard(work_place);
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(work_place[i][j]==1) //cctv 1번(한쪽 방향)
-                {
-                    for(int k=0;k<4;k++)
-                    {
-                        if(k==0)
-                        {
-                            up_dir(newboard, i, j);
-                        }
-                        else if(k==1)
-                        {
-                            down_dir(newboard, i, j, n);
-                        }
-                        else if(k==2)
-                        {
-                            left_dir(newboard, i, j);
-                        }
-                        else if(k==3)
-                        {
-                            left_dir(newboard, i, j);
-                        }
-                        Findmin(newboard, cnt+1);
-                    }
-                }
-                else if(work_place[i][j]==2) //cctv 2번(반대방향)
-                {
-                    for(int k=0;k<2;k++)
-                    {
-                        if(k==0)
-                        {
-                            left_dir(newboard, i, j);
-                            right_dir(newboard, i, j, m);
-                        }
-                        else
-                        {
-                            up_dir(newboard, i, j);
-                            down_dir(newboard, i, j, n);
-                        }
-                    }
-                    Findmin(newboard, cnt+1);
-                }
-                else if(work_place[i][j]==4) //cctv 4번(3방향)
-                {
-                    for(int k=0;k<4;k++)
-                    {
-                        if(k==0)
-                        {
-                            up_dir(newboard, i, j);
-                            right_dir(newboard, i, j, m);
-                            down_dir(newboard, i, j, n);
-                        }
-                        else if(k==1)
-                        {
-                            right_dir(newboard, i, j, m);
-                            down_dir(newboard, i, j, n);
-                            left_dir(newboard, i, j);
-                        }
-                        else if(k==2)
-                        {
-                            down_dir(newboard, i, j, n);
-                            left_dir(newboard, i, j);
-                            up_dir(newboard, i, j);
-                        }
-                        else if(k==3)
-                        {
-                            left_dir(newboard, i, j);
-                            up_dir(newboard, i, j);
-                            right_dir(newboard, i, j, m);
-                        }
-                    }
-                    Findmin(newboard, cnt+1);
-                }
-                else if(work_place[i][j]==3) //cctv 3번(수직방향)
-                {
-                    for(int k=0;k<4;k++)
-                    {
-                        if(k==0)
-                        {
-                            up_dir(newboard, i, j);
-                            right_dir(newboard, i, j, m);
-                        }
-                        else if(k==1)
-                        {
-                            right_dir(newboard, i, j, m);
-                            down_dir(newboard, i, j, n);
-                        }
-                        else if(k==2)
-                        {
-                            down_dir(newboard, i, j, n);
-                            left_dir(newboard, i, j);
-                        }
-                        else if(k==3)
-                        {
-                            left_dir(newboard, i, j);
-                            up_dir(newboard, i, j);
-                        }
-                    }
-                    Findmin(newboard, cnt+1);
-                }
-                else if(work_place[i][j]==5) //cctv 5번(4방향)
-                {
-                    up_dir(newboard, i, j);
-                    right_dir(newboard, i, j, m);
-                    down_dir(newboard, i, j, n);
-                    left_dir(newboard, i, j);
-                    Findmin(newboard, cnt+1);
-                }
+        //현재 처리해야할 cctv
+        int x=cctvs[cnt][0];
+        int y=cctvs[cnt][1];
+        int type=cctvs[cnt][2];
+        int[][] newboard;
+        if (type == 1) {
+            for (int k = 0; k < 4; k++) { // 4 방향
+                newboard = copyboard(work_place);
+                if (k == 0) up_dir(newboard, x, y);
+                else if (k == 1) down_dir(newboard, x, y, n);
+                else if (k == 2) left_dir(newboard, x, y);
+                else if (k == 3) right_dir(newboard, x, y, m);
+                Findmin(newboard, cnt + 1);
             }
+        } else if (type == 2) {
+            for (int k = 0; k < 2; k++) {
+                newboard = copyboard(work_place);
+                if (k == 0) { // 좌우
+                    left_dir(newboard, x, y);
+                    right_dir(newboard, x, y, m);
+                } else { // 상하
+                    up_dir(newboard, x, y);
+                    down_dir(newboard, x, y, n);
+                }
+                Findmin(newboard, cnt + 1);
+            }
+        } else if (type == 3) {
+            for (int k = 0; k < 4; k++) {
+                newboard = copyboard(work_place);
+                if (k == 0) { // 상우
+                    up_dir(newboard, x, y);
+                    right_dir(newboard, x, y, m);
+                } else if (k == 1) { // 우하
+                    right_dir(newboard, x, y, m);
+                    down_dir(newboard, x, y, n);
+                } else if (k == 2) { // 하좌
+                    down_dir(newboard, x, y, n);
+                    left_dir(newboard, x, y);
+                } else if (k == 3) { // 좌상
+                    left_dir(newboard, x, y);
+                    up_dir(newboard, x, y);
+                }
+                Findmin(newboard, cnt + 1);
+            }
+        } else if (type == 4) {
+            for (int k = 0; k < 4; k++) {
+                newboard = copyboard(work_place);
+                if (k == 0) { // 상우하
+                    up_dir(newboard, x, y);
+                    right_dir(newboard, x, y, m);
+                    down_dir(newboard, x, y, n);
+                } else if (k == 1) { // 우하좌
+                    right_dir(newboard, x, y, m);
+                    down_dir(newboard, x, y, n);
+                    left_dir(newboard, x, y);
+                } else if (k == 2) { // 하좌상
+                    down_dir(newboard, x, y, n);
+                    left_dir(newboard, x, y);
+                    up_dir(newboard, x, y);
+                } else if (k == 3) { // 좌상우
+                    left_dir(newboard, x, y);
+                    up_dir(newboard, x, y);
+                    right_dir(newboard, x, y, m);
+                }
+                Findmin(newboard, cnt + 1);
+            }
+        } else if (type == 5) {
+            newboard = copyboard(work_place);
+            up_dir(newboard, x, y);
+            right_dir(newboard, x, y, m);
+            down_dir(newboard, x, y, n);
+            left_dir(newboard, x, y);
+            Findmin(newboard, cnt + 1);
         }
-        return -1;
     }
     public static int[][] copyboard(int[][] board) 
     {
@@ -177,7 +141,7 @@ public class B15683
     }
     public static void up_dir(int[][] work_place,int row,int col) //i,j를 받을때
     {
-        for(int i=0;i<row;i++)
+        for(int i=row-1;i>=0;i--)
         {
             if(work_place[i][col]==6) break; //벽을 만난경우 break
             else if(work_place[i][col]!=6&&work_place[i][col]==0) //벽이 아니고, 빈칸일 경우
@@ -210,7 +174,7 @@ public class B15683
     }
     public static void left_dir(int[][] work_place,int row,int col)
     {
-        for(int i=0;i<col;i++)
+        for(int i=col-1;i>=0;i--)
         {
             if(work_place[row][i]==6) break;
             else if(work_place[row][i]!=6&&work_place[row][i]==0)
--- src/test.java
+++ src/test.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class test {
     public static int cctv_cnt = 0; // CCTV의 개수
     public static int minBlindSpot = Integer.MAX_VALUE; // 최소 사각지대 크기
+    public static List<int[]> cctvs = new ArrayList<>(); // CCTV 위치와 종류
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,14 +15,13 @@ public class test {
             for (int j = 0; j < m; j++) {
                 work_place[i][j] = sc.nextInt();
                 if (work_place[i][j] != 0 && work_place[i][j] != 6) {
+                    cctvs.add(new int[]{i, j, work_place[i][j]}); // CCTV 위치와 종류 저장
                     cctv_cnt++;
                 }
             }
         }
-
         Findmin(work_place, 0);
         System.out.println(minBlindSpot);
-
         sc.close();
     }
 
@@ -42,82 +42,32 @@ public class test {
             return;
         }
 
-        int[][] newboard = copyboard(work_place);
+        // 현재 처리할 CCTV 정보
+        int[] cctv = cctvs.get(cnt);
+        int x = cctv[0];
+        int y = cctv[1];
+        int type = cctv[2];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (work_place[i][j] >= 1 && work_place[i][j] <= 5) {
-                    if (work_place[i][j] == 1) {
-                        for (int k = 0; k < 4; k++) {
-                            copyArray(work_place, newboard);
-                            applyDirection(newboard, i, j, k);
-                            Findmin(newboard, cnt + 1);
-                        }
-                    } else if (work_place[i][j] == 2) {
-                        for (int k = 0; k < 2; k++) {
-                            copyArray(work_place, newboard);
-                            applyDirection(newboard, i, j, k);
-                            applyDirection(newboard, i, j, (k + 2) % 4);
-                            Findmin(newboard, cnt + 1);
-                        }
-                    } else if (work_place[i][j] == 3) {
-                        for (int k = 0; k < 4; k++) {
-                            copyArray(work_place, newboard);
-                            applyDirection(newboard, i, j, k);
-                            applyDirection(newboard, i, j, (k + 1) % 4);
-                            Findmin(newboard, cnt + 1);
-                        }
-                    } else if (work_place[i][j] == 4) {
-                        for (int k = 0; k < 4; k++) {
-                            copyArray(work_place, newboard);
-                            applyDirection(newboard, i, j, k);
-                            applyDirection(newboard, i, j, (k + 1) % 4);
-                            applyDirection(newboard, i, j, (k + 2) % 4);
-                            Findmin(newboard, cnt + 1);
-                        }
-                    } else if (work_place[i][j] == 5) {
-                        copyArray(work_place, newboard);
-                        for (int k = 0; k < 4; k++) {
-                            applyDirection(newboard, i, j, k);
-                        }
-                        Findmin(newboard, cnt + 1);
-                    }
-                    return; // 현재 CCTV 처리 후 종료
-                }
-            }
+        // CCTV 방향 설정
+        int[][] directions;
+        if (type == 1) {
+            directions = new int[][]{{0}, {1}, {2}, {3}}; // 1개 방향
+        } else if (type == 2) {
+            directions = new int[][]{{0, 1}, {2, 3}}; // 반대 방향 2개
+        } else if (type == 3) {
+            directions = new int[][]{{0, 3}, {1, 3}, {1, 2}, {0, 2}}; // 직각 방향 2개
+        } else if (type == 4) {
+            directions = new int[][]{{0, 2, 3}, {0, 1, 3}, {1, 2, 3}, {0, 1, 2}}; // 3개 방향
+        } else { // type == 5
+            directions = new int[][]{{0, 1, 2, 3}}; // 4개 방향
         }
-    }
 
-    public static void applyDirection(int[][] board, int row, int col, int dir) {
-        int n = board.length;
-        int m = board[0].length;
-
-        if (dir == 0) { // 위쪽
-            for (int i = row - 1; i >= 0; i--) {
-                if (board[i][col] == 6) break;
-                if (board[i][col] == 0) board[i][col] = -1;
+        for (int[] dirSet : directions) {
+            int[][] newboard = copyboard(work_place);
+            for (int dir : dirSet) {
+                mark(newboard, x, y, dir);
             }
-        } else if (dir == 1) { // 아래쪽
-            for (int i = row + 1; i < n; i++) {
-                if (board[i][col] == 6) break;
-                if (board[i][col] == 0) board[i][col] = -1;
-            }
-        } else if (dir == 2) { // 왼쪽
-            for (int i = col - 1; i >= 0; i--) {
-                if (board[row][i] == 6) break;
-                if (board[row][i] == 0) board[row][i] = -1;
-            }
-        } else if (dir == 3) { // 오른쪽
-            for (int i = col + 1; i < m; i++) {
-                if (board[row][i] == 6) break;
-                if (board[row][i] == 0) board[row][i] = -1;
-            }
-        }
-    }
-
-    public static void copyArray(int[][] src, int[][] dest) {
-        for (int i = 0; i < src.length; i++) {
-            System.arraycopy(src[i], 0, dest[i], 0, src[0].length);
+            Findmin(newboard, cnt + 1);
         }
     }
 
@@ -126,10 +76,24 @@ public class test {
         int m = board[0].length;
         int[][] newboard = new int[n][m];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                newboard[i][j] = board[i][j];
-            }
+            System.arraycopy(board[i], 0, newboard[i], 0, m);
         }
         return newboard;
+    }
+
+    public static void mark(int[][] board, int x, int y, int dir) {
+        int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
+        int[] dy = {0, 0, -1, 1};
+
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        while (nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length && board[nx][ny] != 6) {
+            if (board[nx][ny] == 0) {
+                board[nx][ny] = -1;
+            }
+            nx += dx[dir];
+            ny += dy[dir];
+        }
     }
 }
