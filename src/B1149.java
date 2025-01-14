03ef0787918dab79ccfe17559d8bccb13aecc0d9
Wonbin <wonbin3977@gmail.com>
2025-01-14 20:47:56 +0900

    B1149

--- src/B1149.java
+++ src/B1149.java
@@ -1,43 +1,36 @@
-import java.util.*;
-import java.io.IOException;
-public class B1149 
+import java.util.Scanner;
+
+public class B1149
 {
-    static int result=Integer.MAX_VALUE;
-    static int temp=0;
-    public static void main(String args[]) throws IOException
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[][] house=new int[n][3]; //rgb칠함
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<3;j++)
-            {
-                house[i][j]=sc.nextInt(); //집의 페인트 가격을 입력받음
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] house = new int[n][3];
+        int[][] dp = new int[n][3]; // dp[i][j]: i번째 집을 j색으로 칠할 때의 최소 비용
+
+        // 입력 받기
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                house[i][j] = sc.nextInt();
             }
         }
-        for(int j=0;j<3;j++)
-        {
-            visiting(house, 0, j, 0);
+
+        // 초기값 설정 (첫 번째 집)
+        dp[0][0] = house[0][0];
+        dp[0][1] = house[0][1];
+        dp[0][2] = house[0][2];
+
+        // DP 테이블 채우기
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = house[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = house[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = house[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
         }
+
+        // 최종 결과
+        int result = Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2]));
         System.out.println(result);
+
         sc.close();
-    }
-    public static void visiting(int[][] house,int n,int color,int cost)
-    {
-        int cnt=house.length;
-        if(n==cnt)
-        {
-            result=Math.min(result,cost);
-            return;
-        }
-        cost+=house[n][color];
-        for(int nextcolor=0;nextcolor<3;nextcolor++)
-        {
-            if(nextcolor!=color)
-            {
-                visiting(house,n+1,nextcolor,cost);
-            }
-        }
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,47 +1,36 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class test
 {
-    public static int index = 0;
-    public static int max = 0;
-    
-    public static void main(String args[])
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long[] number = new long[n]; // 배열 크기를 n으로 설정
-        for (int i = 0; i < n; i++)
-        {
-            number[i] = sc.nextLong(); // long 타입의 값 입력
-        }
-        Arrays.sort(number); // 오름차순으로 정렬
-        
-        int cnt = 1;
-        for (int i = 1; i < n; i++) // 숫자 개수 카운트
-        {
-            if (number[i] == number[i - 1]) // 이전 숫자와 같다면
-            {
-                cnt++;
-            }
-            else
-            {
-                if (cnt > max)
-                {
-                    max = cnt;
-                    index = i - 1; // 최빈값 인덱스 업데이트
-                }
-                cnt = 1; // 카운트 리셋
+        int[][] house = new int[n][3];
+        int[][] dp = new int[n][3]; // dp[i][j]: i번째 집을 j색으로 칠할 때의 최소 비용
+
+        // 입력 받기
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                house[i][j] = sc.nextInt();
             }
         }
-        
-        // 마지막 그룹도 체크
-        if (cnt > max)
-        {
-            max = cnt;
-            index = n - 1;
+
+        // 초기값 설정 (첫 번째 집)
+        dp[0][0] = house[0][0];
+        dp[0][1] = house[0][1];
+        dp[0][2] = house[0][2];
+
+        // DP 테이블 채우기
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = house[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = house[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = house[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
         }
-        
-        System.out.println(number[index]); // 최빈값 출력
+
+        // 최종 결과
+        int result = Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2]));
+        System.out.println(result);
+
         sc.close();
     }
 }
