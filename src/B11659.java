033610209da7ddb4bcd4574256d96c30540ac9a3
Wonbin <wonbin3977@gmail.com>
2025-01-14 21:29:01 +0900

    B11659

--- src/B11659.java
+++ src/B11659.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class test {
+public class B11659 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
