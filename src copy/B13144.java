9a0609ef56cb758083d95606b9bf364b3ca20c5b
wonbin <wonbin3977@gmail.com>
2025-02-03 19:43:09 +0900

    B13144 투 포인터+hashset

--- src copy/B13144.java
+++ src copy/B13144.java
@@ -1,23 +1,30 @@
 import java.io.*;
+import java.util.*;
 public class B13144 
 {
-    static int cnt=0;
     public static void main(String args[]) throws IOException
     {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine()); //수열의 길이 n
         String[] input=br.readLine().split(" ");
         int[] number=new int[n];
-        cnt=n; //일단 하나씩 뽑는 경우 n가지 경우의 수
+        long cnt=0; //경우의 수
         for(int i=0;i<n;i++)
         {
             number[i]=Integer.parseInt(input[i]); //수열 입력받음
         }
-        int en=1;
-        
-    }
-    static boolean is_same(int a, int b) //두 수를 비교하는 메서드
-    {
-        return a == b;
+        int end=0;
+        Set<Integer> set=new HashSet<>();
+        for(int start=0;start<n;start++)
+        {
+            while(end<n&&!set.contains(number[end]))
+            {
+                set.add(number[end]);
+                end++;
+            }
+            cnt+=(end-start);
+            set.remove(number[start]);
+        }
+        System.out.println(cnt);
     }
 }
