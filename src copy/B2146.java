d6ad844ecf4a3d4b13d3a4cc1f8e3a46abf55f11
wonbin <wonbin3977@gmail.com>
2025-04-07 23:08:26 +0900

    B2146 bfs활용

--- src copy/B2146.java
+++ src copy/B2146.java
@@ -8,14 +8,13 @@ public class B2146
     static int n;
     static int island_cnt=1;
     static boolean[][] visited;
-    static int[][][] updated_map; //업데이트된 지도를 저장
+    static int min=Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException
     {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         n=Integer.parseInt(br.readLine());
         map=new int[n][n];
         visited=new boolean[n][n];
-        updated_map=new int[n][n][1];
         for(int i=0;i<n;i++)
         {
             String[] input=br.readLine().split(" ");
@@ -39,9 +38,25 @@ public class B2146
         {
             for(int j=0;j<n;j++)
             {
-                updated_map[i][j][0]=map[i][j];
+                if(map[i][j]!=0)
+                {
+                    int current_island=map[i][j];
+                    for(int k=0;k<4;k++)
+                    {
+                        int cx=i+direction[k][0];
+                        int cy=j+direction[k][1];
+                        if(cx>=0 && cx<n && cy>=0 && cy<n)
+                        {
+                            if(map[cx][cy]==0)
+                            {
+                                min=Math.min(min,bfs(cx, cy,current_island));
+                            }
+                        }
+                    }
+                }
             }
         }
+        System.out.println(min);
     }
     static void bfs_To_Island_cnt(int x,int y)
     {
@@ -68,5 +83,40 @@ public class B2146
             }    
         }
     }
-
+    static int bfs(int x,int y,int current_island)
+    {
+        Queue<int[]> queue=new LinkedList<>();
+        queue.add(new int[] {x,y});
+        boolean[][] v=new boolean[n][n];
+        v[x][y]=true;
+        int cnt=1;
+        while(!queue.isEmpty())
+        {
+            int size=queue.size();
+            for(int j=0;j<size;j++)
+            {
+                int[] node=queue.poll();
+                int cx=node[0]; int cy=node[1];
+                for(int i=0;i<4;i++)
+                {
+                    int nx=cx+direction[i][0];
+                    int ny=cy+direction[i][1];
+                    if(nx>=0 && nx<n && ny>=0 && ny<n)
+                    {
+                        if(!v[nx][ny] && map[nx][ny]==0)
+                        {
+                            v[nx][ny]=true;
+                            queue.add(new int[] {nx,ny});
+                        }
+                        if(map[nx][ny]!=0 && map[nx][ny]!=current_island) //다른 섬에 도착한 경우
+                        {
+                            return cnt;
+                        }
+                    }
+                }
+            }
+            cnt++;
+        }
+        return cnt;
+    }
 }
