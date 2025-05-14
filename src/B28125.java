d5aed6ee69a3a835a13fab30bfb7776572977dde
wonbin <wonbin3977@gmail.com>
2025-05-15 01:01:02 +0900

    B28125 수정

--- src/B28125.java
+++ src/B28125.java
@@ -4,64 +4,60 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class B28125 {
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws IOException 
-    {
+    public static void main(String[] args) throws IOException {
         HashMap<String, String> hm = new HashMap<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String[] s = new String[n]; // 단어들이 주어짐
+        StringBuilder sb = new StringBuilder();
 
-        // 특수 문자와 치환 문자 매핑
+        int n = Integer.parseInt(br.readLine());
+
+        // 특수 문자 → 알파벳 매핑
         hm.put("@", "a");
-        hm.put("[", "c"); // '['는 정규 표현식에서 이스케이프 필요
+        hm.put("[", "c");
         hm.put("!", "i");
         hm.put(";", "j");
-        hm.put("^", "n"); // '^'는 정규 표현식에서 이스케이프 필요
+        hm.put("^", "n");
         hm.put("0", "o");
         hm.put("7", "t");
-        hm.put("\\\'", "v"); // \'
-        hm.put("\\\\\'", "w"); // \\'
 
-        for (int i = 0; i < n; i++) 
-        {
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            StringBuilder result = new StringBuilder();
             int cnt = 0;
-            s[i] = br.readLine();
-            for (int j = 0; j < s[i].length(); ) 
-            {
-                if (j + 2 < s[i].length() && s[i].charAt(j) == '\\' && s[i].charAt(j + 1) == '\\' && s[i].charAt(j + 2) == '\'') 
-                {
+
+            for (int j = 0; j < input.length(); ) {
+                // \\' → w
+                if (j + 2 < input.length() && input.charAt(j) == '\\' && input.charAt(j + 1) == '\\' && input.charAt(j + 2) == '\'') {
+                    result.append('w');
                     cnt++;
                     j += 3;
-                } 
-                else if 
-                (j + 1 < s[i].length() && s[i].charAt(j) == '\\' && s[i].charAt(j + 1) == '\'') 
-                {
+                }
+                // \' → v
+                else if (j + 1 < input.length() && input.charAt(j) == '\\' && input.charAt(j + 1) == '\'') {
+                    result.append('v');
                     cnt++;
                     j += 2;
-                } 
-                else 
-                {
-                    String k = String.valueOf(s[i].charAt(j));
-                    if (hm.containsKey(k)) cnt++;
+                }
+                // 특수문자 → 알파벳
+                else {
+                    String ch = String.valueOf(input.charAt(j));
+                    if (hm.containsKey(ch)) {
+                        result.append(hm.get(ch));
+                        cnt++;
+                    } else {
+                        result.append(ch);
+                    }
                     j++;
                 }
             }
-            s[i] = s[i].replace("\\\\\'", "w");
-            s[i] = s[i].replace("\\'", "v");
-            for (String k : hm.keySet()) 
-            {
-                s[i] = s[i].replace(k, hm.get(k));
-            }
-            int len=s[i].length();
-            // 조건에 따라 결과 저장
-            if (cnt >= len / 2) {
+
+            int len = result.length();
+
+            if (cnt >= (len + 1) / 2) { // 절반 이상이면 이해 못함
                 sb.append("I don't understand\n");
             } else {
-                sb.append(s[i]).append("\n");
+                sb.append(result).append("\n");
             }
-           //sb.append(len).append(" ").append(cnt).append("\n");
         }
 
         System.out.print(sb);
