107ea651d01e474354900d2767eb491452154237
wonbin <wonbin3977@gmail.com>
2026-01-30 17:17:40 +0900

    B18869

--- src/B18869.java
+++ src/B18869.java
@@ -1,90 +1,82 @@
-import java.io.*;
-public class B18869 
-{
-    static int[][] universe;
-    static int[] A_selected;
-    static int[] B_selected;
-    static int n;
-    static int m;
-    public static void main(String[] args) throws IOException
-    {
-        int answer=0;
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String[] input=br.readLine().split(" ");
-        m=Integer.parseInt(input[0]); //우주의 개수
-        n=Integer.parseInt(input[1]); //우주에 있는 행성의 개수
-        universe=new int[m][n];
-        A_selected=new int[n];
-        B_selected=new int[n];
-        for(int i=0;i<m;i++)
-        {
-            input=br.readLine().split(" ");
-            for(int j=0;j<n;j++)
-            {
-                universe[i][j]=Integer.parseInt(input[j]);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class B18869 {
+    static int[][] planetInfo;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int m = Integer.parseInt(input[0]); // 우주의 개수
+        int n = Integer.parseInt(input[1]); // 각 우주에 존재하는 항성의 개수
+        planetInfo = new int[m][n]; // 각 우주에 대한 정보
+        int cnt=0;
+        for(int i=0;i<m;i++) {
+            input = br.readLine().split(" ");
+            for(int j=0;j<n;j++) {
+                int planetSize = Integer.parseInt(input[j]); // 항성의 크기
+                planetInfo[i][j] = planetSize;
             }
         }
-        for(int i=0;i<m;i++)
-        {
-            for(int j=i+1;j<m;j++)
-            {
-                //0이면 쌍둥이 우주, -1이면 x
-                int result=dfs(0,0,i,j);
-                if(result==0)
-                {
-                    answer++;
+        int[][] rankInfo = makeRank(m, n); // 순서가 정해진 배열
+        for(int i=0;i<m-1;i++) {
+            for(int j=i+1;j<m;j++) {
+                if(calculatePair(rankInfo, n, i, j)) {
+                    cnt++;
                 }
             }
         }
-        System.out.println(answer);
+        System.out.println(cnt);
     }
-    static int dfs(int start,int depth,int A_universe,int B_universe)
-    {
-        boolean check=true;
-        if(depth==2)
-        {
-            int a_1=A_selected[0]; int a_2=A_selected[1];
-            int b_1=B_selected[0]; int b_2=B_selected[1];
-            if(a_1-a_2>0) //양수
-            {
-                if(b_1-b_2<=0)
-                {
-                    return -1;
+
+    static int[][] makeRank(int m, int n) {
+        int[][] result = new int[m][n];
+        for(int i=0;i<m;i++) {
+            int[] sorted = planetInfo[i].clone(); // 복제
+            int[] rank = new int[n];
+            for(int j=0;j<n;j++) rank[j] = -1;
+            HashMap<Integer,ArrayList<Integer>> hm = new HashMap<>();
+            int ranking = 1;
+            for(int j=0;j<n;j++) {
+                hm.putIfAbsent(sorted[j], new ArrayList<>());
+                hm.get(sorted[j]).add(j);
+            }
+
+            Arrays.sort(sorted);
+            for(int j=0;j<n;j++) {
+                int current = sorted[j];
+                ArrayList<Integer> al = hm.get(current);
+                if(al.size()!=1) {
+                    for(int num : hm.get(current)) {
+                        if(rank[num]==-1) {
+                            rank[num] = ranking;
+                            break;
+                        }
+                    }
                 }
-                return 0;
-            }
-            else if(a_1-a_2==0) //0
-            {
-                if(b_1-b_2!=0)
-                {
-                    return -1;
+                else {
+                    for(int num : hm.get(current)) { //인덱스값
+                    if(rank[num] == -1) { // 초기값이면 채우고, 아니면 다음
+                        rank[num] = ranking++;
+                        break;
+                    }
                 }
-                return 0;
-            }
-            else //음수
-            {
-                if(b_1-b_2>=0)
-                {
-                    return -1;
                 }
             }
-            return 0;
+            result[i] = rank;
         }
-        for(int i=start;i<n;i++)
-        {
-            A_selected[depth]=universe[A_universe][i];
-            B_selected[depth]=universe[B_universe][i];
-            int result=dfs(i,depth+1,A_universe,B_universe);
-            if(result==-1)
-            {
-                check=false;
-                break;
+        return result;
+    }
+
+    static boolean calculatePair(int[][] rank, int n,int cx, int nx) {
+        for(int i=0;i<n;i++) {
+            if(rank[cx][i] != rank[nx][i]) {
+                return false;
             }
         }
-        if(check)
-        {
-            return 0;
-        }
-        else return -1;
+        return true;
     }
 }
